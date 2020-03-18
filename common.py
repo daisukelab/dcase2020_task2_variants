6c7b3e3bd7cd58458baed881d8ed96b048fe1845
daisukelab <niz.edge@gmail.com>
2020-03-18 16:11:36 +0900

    Add EDA draft.

--- common.py
+++ common.py
@@ -15,9 +15,11 @@ import glob
 import argparse
 import sys
 import os
+from pathlib import Path
+import pandas as pd
 
 # additional
-import numpy
+import numpy as np
 import librosa
 import librosa.core
 import librosa.feature
@@ -106,7 +108,7 @@ def file_load(wav_name, mono=False):
     mono : boolean
         When load a multi channels file and this param True, the returned data will be merged for mono data
 
-    return : numpy.array( float )
+    return : np.array( float )
     """
     try:
         return librosa.load(wav_name, sr=None, mono=mono)
@@ -132,7 +134,7 @@ def file_to_vector_array(file_name,
     file_name : str
         target .wav file
 
-    return : numpy.array( numpy.array( float ) )
+    return : np.array( np.array( float ) )
         vector array
         * dataset.shape = (dataset_size, feature_vector_length)
     """
@@ -149,17 +151,17 @@ def file_to_vector_array(file_name,
                                                      power=power)
 
     # 03 convert melspectrogram to log mel energy
-    log_mel_spectrogram = 20.0 / power * numpy.log10(mel_spectrogram + sys.float_info.epsilon)
+    log_mel_spectrogram = 20.0 / power * np.log10(mel_spectrogram + sys.float_info.epsilon)
 
     # 04 calculate total vector size
     vector_array_size = len(log_mel_spectrogram[0, :]) - frames + 1
 
     # 05 skip too short clips
     if vector_array_size < 1:
-        return numpy.empty((0, dims))
+        return np.empty((0, dims))
 
     # 06 generate feature vectors by concatenating multiframes
-    vector_array = numpy.zeros((vector_array_size, dims))
+    vector_array = np.zeros((vector_array_size, dims))
     for t in range(frames):
         vector_array[:, n_mels * t: n_mels * (t + 1)] = log_mel_spectrogram[:, t: t + vector_array_size].T
 
@@ -218,7 +220,7 @@ def list_to_vector_array(file_list,
         description for tqdm.
         this parameter will be input into "desc" param at tqdm.
 
-    return : numpy.array( numpy.array( float ) )
+    return : np.array( np.array( float ) )
         vector array for training (this function is not used for test.)
         * dataset.shape = (number of feature vectors, dimensions of feature vectors)
     """
@@ -234,7 +236,7 @@ def list_to_vector_array(file_list,
                                             hop_length=hop_length,
                                             power=power)
         if idx == 0:
-            dataset = numpy.zeros((vector_array.shape[0] * len(file_list), dims), float)
+            dataset = np.zeros((vector_array.shape[0] * len(file_list), dims), float)
             logger.info((f'Creating data for {len(file_list)} files: size={dataset.shape[0]}'
                          f', shape={dataset.shape[1:]}'))
         dataset[vector_array.shape[0] * idx: vector_array.shape[0] * (idx + 1), :] = vector_array
@@ -275,7 +277,7 @@ def file_list_generator(target_dir,
 ########################################################################
 
 class ToTensor1d(object):
-    """PyTorch basic transform to convert numpy array to torch.Tensor.
+    """PyTorch basic transform to convert np array to torch.Tensor.
     Args:
         array: (dim,) or (batch, dims) feature array.
     """
